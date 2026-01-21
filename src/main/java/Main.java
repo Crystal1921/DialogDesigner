88f69cab042e8cacbc817a78a1d16d22d7270f2e
Crystal1921 <90130465+Crystal1921@users.noreply.github.com>
2026-01-21 17:59:27 +0800

    init

---
+++ src/main/java/Main.java
@@ -0,0 +1,20 @@
+import imgui.ImGui;
+import imgui.app.Application;
+import imgui.app.Configuration;
+
+public class Main extends Application {
+    @Override
+    protected void configure(Configuration config) {
+        config.setTitle("Dear ImGui is Awesome!");
+    }
+
+    @Override
+    public void process() {
+        ImGui.text("Hello, World!");
+        ImGui.text("This is a test!");
+    }
+
+    public static void main(String[] args) {
+        launch(new Main());
+    }
+}
